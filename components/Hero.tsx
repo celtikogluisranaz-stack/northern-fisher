4c7fd6c7744c8a2fb0e734bf788f70c9a4f8afbf
isra <isranaz.cel@gmail.com>
2026-09-24 20:16:40 -0400

    Fixed Hydration Error part 2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,6 +12,7 @@ export default function Hero() {
         className="inline-block mt-8 bg-orange-500 hover:bg-orange-600 transition-colors text-white font-semibold px-8 py-3 rounded-lg"
       >
         Shop Now 
+
       </a>
     </section>
   )
